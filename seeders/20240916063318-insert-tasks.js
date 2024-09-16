1cbba6fb12a4f731960ccef933ac4215271ced28
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-16 10:17:37 +0300

    Fix seed insert-tasks

--- seeders/20240916063318-insert-tasks.js
+++ seeders/20240916063318-insert-tasks.js
@@ -1,5 +1,5 @@
 'use strict';
-import _ from 'lodash';
+const _ = require('lodash');
 const { User } = require('../models');
 
 /** @type {import('sequelize-cli').Migration} */
@@ -8,19 +8,14 @@ module.exports = {
     const users = await User.findAll({
       attributes: ['id'],
     });
-    // users = [{id:1},{id:2}]
     const tasks = users
       .map((user) => {
-        return new Array(_.random(1, 10)).fill(null).map((e, i) => ({
+        return new Array(_.random(1, 10, false)).fill(null).map((e, i) => ({
           user_id: user.id,
           content: `content for task number(${i}) by user id = ${user.id}`,
           created_at: new Date(),
           updated_at: new Date(),
         }));
-        // tasks = [
-        //[{id:1,user_id:1, content:'content for task by user id = 1'}],
-        //[{id:2,user_id:1,content:'content for task by user id = 1'}]
-        //]
       })
       .flat(2);
 
