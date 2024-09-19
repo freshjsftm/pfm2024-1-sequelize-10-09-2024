8bbd51df4310a02517d8b8d1bba79785a923e9f5
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-19 10:31:36 +0300

    Add getGroupsByUser  & getGroupByUser

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const createError = require('http-errors');
 const attrs = ['name', 'imagePath', 'description'];
 
 module.exports.createGroup = async (req, res, next) => {
@@ -6,7 +7,43 @@ module.exports.createGroup = async (req, res, next) => {
     const { body, userInstance } = req;
     const values = _.pick(body, attrs);
     const newGroup = await userInstance.createGroup(values);
+    if (!newGroup) {
+      return next(createError(400, 'Fix group data '));
+    }
     res.status(201).send({ data: newGroup });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getAllGroups = async (req, res, next) => {
+  try {
+    const { userInstance } = req;
+    const groups = await userInstance.getGroups({
+      through: {
+        where: { groupId: 1 },
+      },
+    });
+    res.status(200).send({ data: groups });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+module.exports.getGroup = async (req, res, next) => {
+  try {
+    const { userInstance, params:{groupId} } = req;
+    const [group] = await userInstance.getGroups({
+      through: {
+        where: { groupId:  groupId},
+      },
+    });
+    if(!group){
+      return next(createError(404, 'Not found'));
+    }
+    // group.dataValues.users_to_groups = undefined;
+    res.status(200).send({ data: group });
   } catch (error) {
     next(error);
   }
--- routes/group.router.js
+++ routes/group.router.js
@@ -1,5 +1,17 @@
 const { Router } = require('express');
-const { createGroup } = require('../controllers/group.controller');
+const {
+  createGroup,
+  getAllGroups,
+  getGroup,
+} = require('../controllers/group.controller');
 const groupRouter = Router();
+// '/users/:userId/groups'
 groupRouter.post('/', createGroup);
+groupRouter.get('/', getAllGroups);
+
+groupRouter.get('/:groupId', getGroup);
+
+//дати нового користувача в групу
+// groupRouter.post('/:groupId', getGroup);
+
 module.exports = groupRouter;
